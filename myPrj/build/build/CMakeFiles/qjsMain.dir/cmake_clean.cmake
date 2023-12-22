b930ce2682e7333b4d4b532c9e46116b88038ad0
cuiyedong <cuiyedong@zepp.com>
2023-12-22 12:08:41 +0800

    feat | hello world

---
+++ myPrj/build/build/CMakeFiles/qjsMain.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/cutils.c.o"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/cutils.c.o.d"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/libbf.c.o"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/libbf.c.o.d"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/libregexp.c.o"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/libregexp.c.o.d"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/libunicode.c.o"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/libunicode.c.o.d"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/myPrj/src/main.c.o"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/myPrj/src/main.c.o.d"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/quickjs-libc.c.o"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/quickjs-libc.c.o.d"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/quickjs.c.o"
+  "CMakeFiles/qjsMain.dir/home/cuiyedong/workspace/QJS/QuickJS/quickjs.c.o.d"
+  "qjsMain"
+  "qjsMain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qjsMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
